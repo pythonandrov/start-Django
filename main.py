801424c2cf0562fc4766ec849238938e27d65448
John <John@yahoo.com>
2022-12-04 02:40:11 +0300

    Writed script for create backand and frontand besout using django

---
+++ main.py
@@ -0,0 +1,31 @@
+﻿from http.server import HTTPServer, BaseHTTPRequestHandler
+
+APP_HOST = 'localhost'
+APP_PORT = 8000
+
+class SimpleGetHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+    def _html(self, message):
+        content = (f'<html>'
+                   f'<body>'
+                   f'<h1>{message}</h1>'
+                   f'</body>'
+                   f'</html>')
+        return content.encode('utf8')
+
+    def do_Get(self):
+        self._set_headers()
+        message = 'Привет Мир'
+        self.wfile.write(self._html(message))
+
+def run_server(server_class = HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_adress = (APP_HOST, APP_PORT)
+    httpd = server_class(server_adress, handler_class)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_server(handler_class=SimpleGetHandler)
